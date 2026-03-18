24e991e74e75a6dbe68bb33ae89c65d6d8fc0325
Jack Anderson <jack.anderson@suitecrm.com>
2026-03-18 08:07:51 +0000

    [Legacy] Bump version to 8.9.3

--- public/legacy/suitecrm_version.php
+++ public/legacy/suitecrm_version.php
@@ -3,6 +3,6 @@ if (!defined('sugarEntry') || !sugarEntry) {
     die('Not A Valid Entry Point');
 }
 
-$suitecrm_version = '8.9.2';
+$suitecrm_version = '8.9.3';
 $suitecrm_timestamp = '2026-03-19 12:00:00';
 $suitecrm_legacy = '7.14.9';
